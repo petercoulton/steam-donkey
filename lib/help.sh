5f00dabfeaf99c43c207fc4af3266e56b9f225f0
Peter Coulton <petercoulton@gmail.com>
2016-11-08 14:07:41 +0000

    Fix lib path in help

--- lib/help.sh
+++ lib/help.sh
@@ -5,7 +5,7 @@ usage: donkey <command>
 Available commands:
 EOF
 
-	(for LIB in $(find ./lib -type f -name '*.sh' | xargs -n1 -I@ basename @ .sh); do
+	(for LIB in $(find ${DONKEY_LIB} -type f -name '*.sh' | xargs -n1 -I@ basename @ .sh); do
 		source "${DONKEY_LIB}/${LIB}.sh"
 		printf "   "
 		eval "donkey-${LIB}-info"
