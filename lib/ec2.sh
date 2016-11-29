c1431c60053003a957c9a2c65a19660e7d95de2e
Peter Coulton <petercoulton@gmail.com>
2016-11-29 20:09:33 +0000

    Filter shared files in lib

---
+++ lib/ec2.sh
@@ -0,0 +1,8 @@
+source "${DONKEY_LIB}/_common.sh"
+
+
+
+
+function donkey-ec2() {
+	echo "steam-donkey ec2"
+}
--- lib/help.sh
+++ lib/help.sh
@@ -1,21 +1,28 @@
 function donkey-help() {
-	cat <<EOF
+    cat <<EOF
 usage: donkey <command>
 
 Options:
-	--profile
-	--region
+    --profile
+    --region
 
 Available commands:
 EOF
 
-	(for LIB in $(find ${DONKEY_LIB} -type f -name '*.sh' | xargs -n1 -I@ basename @ .sh); do
-		source "${DONKEY_LIB}/${LIB}.sh"
-		printf "   "
-		eval "donkey-${LIB}-info"
-	done) | column -c 80
+    (for LIB in $(find ${DONKEY_LIB} -type f -name '*.sh' | xargs -n1 -I@ basename @ .sh); do
+        source "${DONKEY_LIB}/${LIB}.sh"
+        printf "   "
+        INFO_FUNCTION="donkey-${LIB}-info"
+
+        if [ -n "$(type -t ${INFO_FUNCTION})" ] && \
+           [ "$(type -t ${INFO_FUNCTION})" = function ]; then
+        
+          eval ${INFO_FUNCTION}
+        fi
+
+    done) | column -c 80
 }
 
 function donkey-help-info() {
-	echo "help"
+    echo "help"
 }
